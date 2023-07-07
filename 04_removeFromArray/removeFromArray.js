bc9551adadebad604b07b7ba60043adada34df50
Eric <anderson.ericandrew@gmail.com>
2023-07-07 11:02:13 -0700

    complete code for problem 4 after passing testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeThis) {
+    let newArray = [];
+    array.forEach(element => {
+        if (element !== removeThis) {
+            newArray.push(element)
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line

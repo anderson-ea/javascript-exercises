73ab2cc71845bd7e637c1f06bf83ddbe0baff522
Eric <anderson.ericandrew@gmail.com>
2023-07-07 11:11:12 -0700

    complete code for problem 5 after passing testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1]
+    }
+    for (let i = num1; i <= num2; i++) {
+        finalSum += i
+    }
+    return finalSum
 };
 
+// if num1 is bigger than num2, switch them
+// for loop, starting at lower number ending at bigger number
+// inside loop add iteration
 // Do not edit below this line
 module.exports = sumAll;

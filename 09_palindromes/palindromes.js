75be3c8d3d2656a95e525ca4f9cab665366569d6
Eric <anderson.ericandrew@gmail.com>
2023-07-08 10:20:22 -0700

    complete code for problem 9 after passing testing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[!?,. ]/g, "").toUpperCase();
+    let compareString = ""
+    for (let i = newString.length - 1; i > -1; i--) {
+        compareString += newString[i]
+    }
+    return compareString === newString
 };
 
 // Do not edit below this line

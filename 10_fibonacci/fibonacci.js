004f6f2681ed9a8cc4a74b3aa7677cc038620371
Eric <anderson.ericandrew@gmail.com>
2023-07-08 11:28:45 -0700

    complete code for problem 10 after passing testing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    let newPlace = parseInt(place)
+    if (newPlace < 0) {
+        return "OOPS"
+    }
+    let a = 1
+    let b = 1
+    while (newPlace > 2) {
+        let c = a + b
+        b = a
+        a = c
+        newPlace -= 1
+    }
+    return a
 };
+
+fibonacci(6) // 6th number in sequence
+// expect 8 to be answer - ex: 1,1,2,3,5,8
 
 // Do not edit below this line
 module.exports = fibonacci;

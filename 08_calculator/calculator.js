d6ff848def20655a692741fd32756648e0307b09
Eric <anderson.ericandrew@gmail.com>
2023-07-08 09:47:19 -0700

    remove test skipping on tempConversion.spec.js and complete all functions that were failing tests for calulator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let totalSum = 0
+	array.forEach(i => {
+    totalSum += i
+  });
+  return totalSum
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let total = 1
+  for (let i = 0; i < args.length; i++) {
+    total *= args[i]
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1 
+  if (num === 0) {
+    return result
+  }   
+  for (let i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line

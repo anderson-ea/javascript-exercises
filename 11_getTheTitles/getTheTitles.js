25ec015197914e6981bb941cc651993225a48ca6
Eric <anderson.ericandrew@gmail.com>
2023-07-08 12:00:05 -0700

    complete code for problem 11 after passing testing

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArray) {
+    let newArray = []
+    booksArray.forEach(book => {
+        newArray.push(book.title)
+    });
+    return newArray
 };
 
 // Do not edit below this line

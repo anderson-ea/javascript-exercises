4696f34ee02df6970c1688b6dcdb4cb38db60396
Eric <anderson.ericandrew@gmail.com>
2023-07-08 12:07:36 -0700

    complete code for problem 12 after passing testing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestYear = 10000000
+    let oldestPerson = {}
+    array.map(person => {
+        if (person.yearOfBirth < oldestYear) {
+            oldestYear = person.yearOfBirth
+            oldestPerson = person
+        }
+    })
+    return oldestPerson
 };
 
 // Do not edit below this line
